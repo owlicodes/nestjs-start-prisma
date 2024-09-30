19961ddf4e83e6e73e9cc1845d1d0a45e9de2f74
owlicodes <owlicodes@gmail.com>
2024-09-30 09:29:32 +0800

    Completing UT for Auth Service.

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -6,6 +6,7 @@ import * as bcrypt from "bcrypt";
 import { AuthService } from "./auth.service";
 import { UsersService } from "../users/users.service";
 import { AppConfigService } from "../config/config.service";
+import { BadRequestException } from "@nestjs/common";
 
 describe("AuthService", () => {
   let authService: AuthService;
@@ -13,6 +14,13 @@ describe("AuthService", () => {
   let jwtService: JwtService;
 
   const mockUser = { email: "test@email.com", password: "testPassword" };
+  const mockResolvedUser = {
+    ...mockUser,
+    id: 0,
+    createdAt: new Date(),
+    password: "testpassword",
+  };
+  const payloadData = { user: { sub: 0, email: "test@email.com" } };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -58,19 +66,46 @@ describe("AuthService", () => {
 
   describe("login", () => {
     it("should call the jwtService sign method twice, 1 for access token and 1 for refresh token.", async () => {
-      jest.spyOn(usersService, "getUserByEmail").mockResolvedValue({
-        ...mockUser,
-        id: 0,
-        createdAt: new Date(),
-        password: "testpassword",
-      });
-
+      jest
+        .spyOn(usersService, "getUserByEmail")
+        .mockResolvedValue(mockResolvedUser);
       jest.spyOn(bcrypt, "compare").mockResolvedValue(true);
 
       await authService.login(mockUser);
 
       expect(jwtService.sign).toHaveBeenCalled();
       expect(jwtService.sign).toHaveBeenCalledTimes(2);
+    });
+
+    it("should throw a BadRequestException if the user already exists.", async () => {
+      jest.spyOn(usersService, "getUserByEmail").mockResolvedValue(null);
+
+      await expect(authService.login(mockUser)).rejects.toThrow(
+        BadRequestException,
+      );
+
+      expect(jwtService.sign).not.toHaveBeenCalled();
+    });
+
+    it("should throw BadRequestException if passwords do not match", async () => {
+      jest
+        .spyOn(usersService, "getUserByEmail")
+        .mockResolvedValue(mockResolvedUser);
+      jest.spyOn(bcrypt, "compare").mockResolvedValue(false);
+
+      await expect(authService.login(mockUser)).rejects.toThrow(
+        BadRequestException,
+      );
+
+      expect(jwtService.sign).not.toHaveBeenCalled();
+    });
+  });
+
+  describe("refresh", () => {
+    it("should sign a new token", async () => {
+      await authService.refresh(payloadData);
+
+      expect(jwtService.sign).toHaveBeenCalled();
     });
   });
 });
